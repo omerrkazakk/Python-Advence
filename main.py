1af48a61eb6cbe0df11392f280bff07c557716f8
Ömer Kazak <omerkazak1441@gmail.com>
2026-08-01 00:00:31 +0300

    An introduction to advanced Python topics has been made.

---
+++ main.py
@@ -0,0 +1,18 @@
+#DECORATORS
+print("-"*60)
+print("Section 1: Decorators")
+print("-"*60)
+
+def my_decorator(func):
+    def wrapper():
+        print("wrapper function executed")
+        func()
+        print("wrapper function executed")
+    return wrapper
+
+@my_decorator
+def hello_world():
+    print("hello world")
+
+
+hello_world()
